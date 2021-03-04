d469adacee216bc0a9fc49a9764554b754395aab
Qais-S <qais-sarfraz@hotmail.co.uk>
2021-03-04 23:49:43 +0000

    Assignment

---
+++ App.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import HomeScreen from './screens/HomeScreen'
+import { Text, TextInput, View, Button, StyleSheet, Alert } from 'react-native';
+
+/*
+class HelloWorldApp extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    );
+  }
+}
+
+export default HelloWorldApp; */
+
+class App extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+   
+     );
+  }
+
+}
+
+const styles = StyleSheet.create({
+});
+
+export default App
+
+
